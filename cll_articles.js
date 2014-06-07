377a46fe61f5b23cd5c7f5ea32eba847df8db6eb
technancy <nancytechy@gmail.com>
2014-06-07 20:58:22 +0100

    add and remove articles

---
+++ cll_articles.js
@@ -0,0 +1,34 @@
+Articles = new Meteor.Collection('Articles');
+
+if (Meteor.isClient) {
+  Template.article.listArticles = function () {
+    return Articles.find();
+  };
+
+  Template.article.events({
+
+    'click #addArticle': function (evt, templ) {
+      Articles.insert({
+        content: 'Enter your text here, and begin highlighting.'
+      });
+    },
+
+    'click .edit': function (evt, templ) {
+      var art = document.getElementById(this._id);
+      Meteor.annoFunct.setEndOfContenteditable(art);
+    },
+
+    'click .delete': function (evt, templ) {
+      var article = templ.find("#"+this._id).value;
+      Articles.remove(this._id);
+    },
+
+    'click .save': function (evt, templ) {
+      var innerArt = document.getElementById(this._id).innerHTML;
+      Articles.update(this._id, {
+        $set: {content: innerArt}
+      });
+    }
+
+  });
+}
