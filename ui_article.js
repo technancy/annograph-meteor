8abb2aa6dae55a61526178e6a32b6bdced106ec6
technancy <nancytechy@gmail.com>
2014-10-24 16:46:08 +0100

    updates previously missed

--- ui_article.js
+++ ui_article.js
@@ -5,10 +5,10 @@ if (Meteor.isClient) {
   Template.article.events({
     'mouseup .article': function (evt, templ) {
       var selection = Meteor.annoUI.grabMousedSelection();
-      console.log(selection);
+      console.log(selection + 'this is templ ' + templ);
       Meteor.annoUI.generateToolTip(evt);
     },
-    'mousedown .article': function (evt) {
+    'mousedown .article': function (evt, templ) {
       var option = Meteor.annoUI.anticipateOption();
       console.log(option);
       Meteor.annoUI.toggleToolTip();
